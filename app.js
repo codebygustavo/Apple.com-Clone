f7340c5790d7e92a66edcb2e280d83e83130438d
Gustavo Couto <116754904+codebygustavo@users.noreply.github.com>
2023-12-13 19:07:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        speed: 0,
+    });
+});
