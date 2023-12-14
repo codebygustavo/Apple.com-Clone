4272ab486f383f4e98f8d680ac4faa0cba859d92
codebygustavo <hbw3.gustavo@gmail.com>
2023-12-13 22:03:20 -0300

    menu

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+
+function toggleMenu() {
+    const e = document.querySelector(".menu");
+    const m = document.querySelector(".menuButton");
+    e.classList.toggle("menu-show");
+    m.classList.toggle("close");
+}
+
+
 $(document).ready(function () {
     $('.carousel').slick({
         autoplay: false,
